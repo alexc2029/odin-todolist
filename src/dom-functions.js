2572a90af87aadc61b99c65aee78e84307805e38
alexc2029 <114008402+alexc2029@users.noreply.github.com>
2025-08-16 17:55:32 +0300

    Implement collapsible todo item details

--- src/dom-functions.js
+++ src/dom-functions.js
@@ -1,5 +1,6 @@
 import tagIcon from "./icons/tag.svg";
 import pinIcon from "./icons/pin.svg";
+import { addCollapsibleInfoListeners } from "./event-listeners";
 
 export function displaySidebarTodoList(todoList) {
 	const sidebar = document.getElementById("sidebar-todolists");
@@ -26,6 +27,7 @@ export function displayTodoItems(todoList) {
 		const card = document.createElement("div");
 
 		const titleContainer = document.createElement("div");
+		titleContainer.className = "show-collapsible-info";
 
 		const title = document.createElement("h2");
 		title.textContent = item.title;
@@ -39,9 +41,22 @@ export function displayTodoItems(todoList) {
 		const dueDate = document.createElement("div");
 		dueDate.textContent = item.dueDate;
 
+		const collapsibleInfo = document.createElement("div");
+		collapsibleInfo.className = "collapsible-info";
+
+		const description = document.createElement("div");
+		description.textContent = item.description;
+		const priority = document.createElement("div");
+		priority.textContent = item.priority;
+
+		collapsibleInfo.appendChild(description);
+		collapsibleInfo.appendChild(priority);
+
 		card.appendChild(titleContainer);
 		card.appendChild(dueDate);
+		card.append(collapsibleInfo);
 
 		main.appendChild(card);
 	}
+	addCollapsibleInfoListeners();
 }
--- src/event-listeners.js
+++ src/event-listeners.js
@@ -5,3 +5,23 @@ export function addViewTodolistListener(todoListContainer, todoList) {
 		displayTodoItems(todoList);
 	});
 }
+
+export function addCollapsibleInfoListeners() {
+	let collapsibles = document.getElementsByClassName("show-collapsible-info");
+	for (let collapsible of collapsibles) {
+		collapsible.addEventListener("click", function () {
+			this.classList.toggle("active");
+			let content = this.nextElementSibling.nextElementSibling;
+			if (content.style.visibility === "visible") {
+				content.style.visibility = "hidden";
+			} else {
+				content.style.visibility = "visible";
+			}
+			if (content.style.maxHeight) {
+				content.style.maxHeight = null;
+			} else {
+				content.style.maxHeight = content.scrollHeight + "px";
+			}
+		});
+	}
+}
